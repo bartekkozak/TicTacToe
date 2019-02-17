fd2c79e63fa4bcd4e6b3e4e6a7dcec69928da9f7
bartek <bartekkozak3@gmail.com>
2019-02-17 12:31:37 +0100

    Updates

---
+++ precache-manifest.31521cefbe6deb557d4a4e071892a575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "92291496055eec84470c",
+    "url": "/TicTacToe/static/js/runtime~main.92291496.js"
+  },
+  {
+    "revision": "50f201b35ce7cdc0a2d5",
+    "url": "/TicTacToe/static/js/main.50f201b3.chunk.js"
+  },
+  {
+    "revision": "2d46e02eb8f40a90035f",
+    "url": "/TicTacToe/static/js/2.2d46e02e.chunk.js"
+  },
+  {
+    "revision": "50f201b35ce7cdc0a2d5",
+    "url": "/TicTacToe/static/css/main.31323ca9.chunk.css"
+  },
+  {
+    "revision": "ab4d7aa475051412afe3a1acc84bb5f4",
+    "url": "/TicTacToe/index.html"
+  }
+];
